781349e3152776eb1bbc684856dd411ed0683411
Marcus Ortiz <mportiz08@gmail.com>
2012-11-28 17:43:05 -0800

    Added "squares test".

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,17 @@
 require 'test/unit'
 require 'stringio'
 
+class String
+  # Strip leading whitespace from each line that is the same as the 
+  # amount of whitespace on the first line of the string.
+  # Leaves _additional_ indentation on later lines intact.
+  # 
+  # from http://stackoverflow.com/a/5638187/94751
+  def unindent
+    gsub /^#{self[/\A\s*/]}/, ''
+  end
+end
+
 def mock_stdout(&block)
   orig_stdout = $stdout
   $stdout = StringIO.new
