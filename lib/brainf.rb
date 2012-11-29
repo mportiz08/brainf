0c3e359376caa275d01552e5ed8d3a774720b6a1
Marcus Ortiz <mportiz08@gmail.com>
2012-11-29 13:12:29 -0800

    Some refactoring.

--- lib/brainf.rb
+++ lib/brainf.rb
@@ -1,17 +1,21 @@
 module BrainF
-  class Interpreter    
+  class Interpreter
+    INVALID_INSTRUCTION        = /[^<>+-.,\[\]]/
+    MIN_BYTE_VAL, MAX_BYTE_VAL = 0, 255
+    MIN_ADDRESS, MAX_ADDRESS   = 0, 30000
+    
     def eval(input)
-      @instructions = input
-      @data         = (1..30000).map { 0 }
-      @iptr, @dptr  = 0, 0
+      @instructions = input.gsub INVALID_INSTRUCTION, ''
+      @data         = (MIN_ADDRESS...MAX_ADDRESS).map { 0 }
+      @iptr, @dptr  = MIN_ADDRESS, MIN_ADDRESS
       @stack        = []
       
       while @iptr < @instructions.size
         case @instructions[@iptr]
         when '>'
-          incr_ptr
+          incr_dptr
         when '<'
-          decr_ptr
+          decr_dptr
         when '+'
           incr_byte
         when '-'
@@ -29,29 +33,47 @@ module BrainF
         @iptr += 1
       end
       
-      @data.map(&:chr).join('')
+      true
     end
     
     private
     
-    def incr_ptr
+    def byte
+      @data[@dptr]
+    end
+    
+    def instruction
+      @instructions[@iptr]
+    end
+    
+    def incr_iptr
+      @iptr += 1
+    end
+    
+    def decr_iptr
+      @iptr -= 1
+    end
+    
+    def incr_dptr
       @dptr += 1
     end
     
-    def decr_ptr
+    def decr_dptr
       @dptr -= 1
     end
     
     def incr_byte
       @data[@dptr] += 1
+      @data[@dptr] = MIN_BYTE_VAL if byte > MAX_BYTE_VAL
     end
     
     def decr_byte
       @data[@dptr] -= 1
+      @data[@dptr] = MAX_BYTE_VAL if byte < MIN_BYTE_VAL
     end
     
     def output_byte
-      print @data[@dptr].chr
+      $stdout.print byte.chr
     end
     
     def accept_byte
@@ -59,24 +81,24 @@ module BrainF
     end
     
     def jump_forward
-      return if @data[@dptr] != 0
+      return unless byte == MIN_BYTE_VAL
       
       @stack.push '['
-      while !@stack.empty?
-        @iptr += 1
-        @stack.push '[' if @instructions[@iptr] == '['
-        @stack.pop      if @instructions[@iptr] == ']'
+      until @stack.empty?
+        incr_iptr
+        @stack.push '[' if instruction == '['
+        @stack.pop      if instruction == ']'
       end
     end
     
     def jump_back
-      return if @data[@dptr] == 0
+      return if byte == MIN_BYTE_VAL
       
       @stack.push ']'
-      while !@stack.empty?
-        @iptr -= 1
-        @stack.push ']' if @instructions[@iptr] == ']'
-        @stack.pop      if @instructions[@iptr] == '['
+      until @stack.empty?
+        decr_iptr
+        @stack.push ']' if instruction == ']'
+        @stack.pop      if instruction == '['
       end
     end
   end
